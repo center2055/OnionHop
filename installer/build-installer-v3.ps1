ea64495d134d97d48ebb3b810a1abd2850539848
center2055 <144978980+center2055@users.noreply.github.com>
2026-08-02 20:46:12 +0200

    v3.7.7: bump version + finalize changelog

--- installer/build-installer-v3.ps1
+++ installer/build-installer-v3.ps1
@@ -194,7 +194,7 @@ if (!(Test-Path $iss)) {
   throw "Missing Inno Setup script: $iss"
 }
 
-$version = "3.7.6"
+$version = "3.7.7"
 try {
   $xml = [xml](Get-Content $csproj)
   $pv = $xml.Project.PropertyGroup.Version
