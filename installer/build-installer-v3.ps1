1ca4ee8689278c8fbc7b275035b84dfbc3ab1d6a
center2055 <144978980+center2055@users.noreply.github.com>
2026-08-21 14:58:20 +0200

    v3.8.1: bump version + finalize changelog

--- installer/build-installer-v3.ps1
+++ installer/build-installer-v3.ps1
@@ -194,7 +194,7 @@ if (!(Test-Path $iss)) {
   throw "Missing Inno Setup script: $iss"
 }
 
-$version = "3.8"
+$version = "3.8.1"
 try {
   $xml = [xml](Get-Content $csproj)
   $pv = $xml.Project.PropertyGroup.Version
